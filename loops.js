9a3bd2505fe1beb385d5aa35689e14c70fd3544d
Claire Muller <clairermuller@gmail.com>
2018-12-23 22:53:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,8 +2,10 @@
    for (let i = 0; i < 25; i++) {
      if (i === 1) {
        return "I am 1 strange loop.";
-     } else {
+     } else if (i < 25) {
        return "I am ${i} strange loops.";
+     } else {
+       return "Error!";
      }
    }
  };
