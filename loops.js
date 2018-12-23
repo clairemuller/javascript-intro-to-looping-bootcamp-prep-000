1ddc83a207875d6ef4878e8bd405c9f362e6fbcf
Claire Muller <clairermuller@gmail.com>
2018-12-23 23:04:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
  const forLoop = (array) => {
    for (let i = 0; i < 25; i++) {
      if (i === 1) {
-       "I am 1 strange loop.";
+       `I am 1 strange loop.`;
      } else {
-       "I am ${i} strange loops.";
+       `I am ${i} strange loops.`;
      }
    }
    return array;
